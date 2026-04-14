15ae36a82d01de3df9df5a5bb9f4aa23ba256104
Abielon <lushen.abdiel@gmail.com>
2026-04-14 12:48:08 -0500

    1.49

--- docs/Lightender.UE4.js
+++ docs/Lightender.UE4.js
@@ -75,7 +75,7 @@ var Module = {
 	assetDownloadProgress: {}, // Track how many bytes of each needed asset has been downloaded so far.
 
 	UE4_indexedDBName: 'UE4_assetDatabase_Lightender', // this should be an ascii ID string without special characters that is unique to the project that is being packaged
-	UE4_indexedDBVersion: 202604011755, // Bump this number to invalidate existing IDB storages in browsers.
+	UE4_indexedDBVersion: 202604141741, // Bump this number to invalidate existing IDB storages in browsers.
 };
 
 
